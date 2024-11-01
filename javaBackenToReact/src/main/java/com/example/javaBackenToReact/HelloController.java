7c95ffe844c1fe8181007d4b54318c4b4b3a7ec8
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-01 22:13:08 +0100

    update

--- javaBackenToReact/src/main/java/com/example/javaBackenToReact/HelloController.java
+++ javaBackenToReact/src/main/java/com/example/javaBackenToReact/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @CrossOrigin(origins = "https://zindriana.github.io/react_to_java/")
+    @CrossOrigin(origins = "https://zindriana.github.io/react_to_java")
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
