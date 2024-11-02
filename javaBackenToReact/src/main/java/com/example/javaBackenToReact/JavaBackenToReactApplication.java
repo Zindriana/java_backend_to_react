b02207c304efb6af392d2881f4af83f29356d61c
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-02 16:03:00 +0100

    Application needed to be on top level, moved it there

--- javaBackenToReact/src/main/java/com/example/javaBackenToReact/JavaBackenToReactApplication.java
+++ javaBackenToReact/src/main/java/com/example/javaBackenToReact/JavaBackenToReactApplication.java
@@ -1,4 +1,4 @@
-package com.example.javaBackenToReact.admin;
+package com.example.javaBackenToReact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
