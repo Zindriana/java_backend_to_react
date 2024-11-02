89369c553f743260f6a10c47e234c60e2273d907
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-02 16:51:50 +0100

    added getters and setters

--- javaBackenToReact/src/main/java/com/example/javaBackenToReact/models/Character.java
+++ javaBackenToReact/src/main/java/com/example/javaBackenToReact/models/Character.java
@@ -15,4 +15,44 @@ public class Character {
         this.spirit = spirit;
         this.social = social;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMental() {
+        return mental;
+    }
+
+    public void setMental(int mental) {
+        this.mental = mental;
+    }
+
+    public int getPhysical() {
+        return physical;
+    }
+
+    public void setPhysical(int physical) {
+        this.physical = physical;
+    }
+
+    public int getSocial() {
+        return social;
+    }
+
+    public void setSocial(int social) {
+        this.social = social;
+    }
+
+    public int getSpirit() {
+        return spirit;
+    }
+
+    public void setSpirit(int spirit) {
+        this.spirit = spirit;
+    }
 }
