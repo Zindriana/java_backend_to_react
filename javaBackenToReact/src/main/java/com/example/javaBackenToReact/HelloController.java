7ff5a0a3b7f5c926e45f2e4983acb6f984e88f76
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-04 15:04:14 +0100

    added Mappings for add and get scrolls

--- javaBackenToReact/src/main/java/com/example/javaBackenToReact/HelloController.java
+++ javaBackenToReact/src/main/java/com/example/javaBackenToReact/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.javaBackenToReact;
 import com.example.javaBackenToReact.lists.HeroList;
+import com.example.javaBackenToReact.lists.ScrollList;
 import com.example.javaBackenToReact.models.Hero;
 import com.example.javaBackenToReact.models.Randomizer;
+import com.example.javaBackenToReact.models.Scroll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +12,12 @@ import java.util.List;
 @RestController
 public class HelloController {
     private final HeroList heroList;
+    private final ScrollList scrollList;
 
     @Autowired
-    public HelloController(HeroList heroList) {
+    public HelloController(HeroList heroList, ScrollList scrollList) {
         this.heroList = heroList;
+        this.scrollList = scrollList;
     }
 
     @CrossOrigin(origins = "https://zindriana.github.io/")
@@ -34,8 +38,16 @@ public class HelloController {
         heroList.addHero(hero);
     }
 
+    @PostMapping("/newscroll")
+    public void newScroll(@RequestBody Scroll scroll) {
+        scrollList.addScroll(scroll);
+    }
+
     @GetMapping("/getcharacters")
     public List<Hero> getCharacters() {
         return heroList.getHeroes();
     }
+
+    @GetMapping("/getscrolls")
+    public List<Scroll> getScrolls() { return scrollList.getScrolls(); }
 }
